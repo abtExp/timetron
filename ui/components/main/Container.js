8d2737929780f21ebff38e84cc9b554ded2ca261
abtExp <atworkstudios@gmail.com>
2017-10-18 17:01:34 +0530

    fixing the sync

--- ui/components/main/Container.js
+++ ui/components/main/Container.js
@@ -12,7 +12,7 @@ class Container extends React.Component{
     constructor(){
         super();
         this.state = {
-            formShow : 'flex',
+            formShow : 'none',
             timers : []
         };
     }
